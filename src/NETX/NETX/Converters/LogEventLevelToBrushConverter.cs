79965feef04de537669e805bb94bcfa05401b3e3
vrykolakas166 <hhtb.bdh@gmail.com>
2025-03-22 23:13:24 +0700

    add

---
+++ src/NETX/NETX/Converters/LogEventLevelToBrushConverter.cs
@@ -0,0 +1,27 @@
+﻿using Serilog.Events;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace NETX.Converters;
+
+public class LogEventLevelConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return value switch
+        {
+            LogEventLevel.Verbose => new SolidColorBrush(Colors.Gray),
+            LogEventLevel.Debug => new SolidColorBrush(Colors.DarkGray),
+            LogEventLevel.Warning => new SolidColorBrush(Colors.Goldenrod),
+            LogEventLevel.Error => new SolidColorBrush(Colors.HotPink),
+            LogEventLevel.Fatal => new SolidColorBrush(Colors.Red),
+            LogEventLevel.Information or _ => new SolidColorBrush(Colors.DeepSkyBlue),
+        };
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ src/NETX/NETX/Views/Pages/SettingPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using CommunityToolkit.Mvvm.DependencyInjection;
+using NETX.ViewModels;
+using System.Windows.Controls;
+
+namespace NETX.Views.Pages
+{
+    /// <summary>
+    /// Interaction logic for SettingPage.xaml
+    /// </summary>
+    public partial class SettingPage : Page
+    {
+        public SettingPage()
+        {
+            InitializeComponent();
+            DataContext = Ioc.Default.GetService<SettingPageViewModel>();
+        }
+    }
+}
